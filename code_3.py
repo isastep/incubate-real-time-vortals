3ee9e6950a53a6c5ae0e544b431d3195cd01870f
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2023-10-30 20:20:38 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,25 @@
+            System.out.println("Random Number: " + item);
+        }
+
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+    public static void main(String[] args) {
+
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
+public class RandomDataGenerator {
+    }        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+    }
+        }
+
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
